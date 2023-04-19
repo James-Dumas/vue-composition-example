f38fca352539e215df1f990cdb967c1b7a110d4a
James Dumas <jamesdumas418@gmail.com>
2023-04-19 15:38:42 -0400

    updated components to script setup

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed } from 'vue'
+
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        required: true
+    }
+})
+
+const inputText = computed({
+    get() {
+        return props.modelValue
+    },
+    set(value) {
+        emit('update:modelValue', value)
+    }
+})
+</script>
+
+<template>
+    <div>
+        <p>{{ props.label }}</p>
+        <input v-model="inputText">
+    </div>
+</template>
