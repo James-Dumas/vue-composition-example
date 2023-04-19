786b9022a034acd810feceaad1dac0edfc3854b3
James Dumas <jamesdumas418@gmail.com>
2023-04-19 15:27:10 -0400

    fix validator

---
+++ src/composables/validator.js
@@ -0,0 +1,32 @@
+import { ref, isRef, unref, watchEffect } from 'vue'
+
+export function useValidator(value) {
+    const isValid = ref(false)
+    const errorMessage = ref(null)
+
+    function validate(value) {
+        if (value.length == 0) {
+            isValid.value = false
+            errorMessage.value = 'Input is required'
+        } else if (value.length < 8) {
+            isValid.value = false
+            errorMessage.value = 'Input is too short'
+        } else if (value.length > 20) {
+            isValid.value = false
+            errorMessage.value = 'Input is too long'
+        } else {
+            isValid.value = true
+            errorMessage.value = null
+        }
+    }
+    
+    if (isRef(value)) {
+        watchEffect(() => {
+            validate(unref(value))
+        })
+    } else {
+        validate(value)
+    }
+    
+    return { isValid, errorMessage }
+}
